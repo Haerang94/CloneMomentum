19d8775229a6139ccb161ab601578755c2bcd8ed
Haerang Choi <gofkdvjvl11@gmail.com>
2019-09-14 14:22:09 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -28,7 +28,7 @@ function paintToDo(text){
     const delBtn=document.createElement("button");
     const span=document.createElement("span");
     const newId=toDos.length+1;
-    delBtn.innerHTML="Delete";
+    delBtn.innerHTML="❌ ";
     delBtn.addEventListener("click",deleteToDo);
     span.innerText=text;
     li.appendChild(delBtn);
